446a6304b4fd74d39b91442f45cee325943628cf
goodven152 <goodven152@gmail.com>
2024-01-24 18:21:20 +0300

    test

---
+++ bin/samp.dart
@@ -0,0 +1,5 @@
+import 'package:samp/samp.dart' as samp;
+
+void main(List<String> arguments) {
+  print('Hello world: ${samp.calculate()}!');
+}
---
+++ test/samp_test.dart
@@ -0,0 +1,8 @@
+import 'package:samp/samp.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
