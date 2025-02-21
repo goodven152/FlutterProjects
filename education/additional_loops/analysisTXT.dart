a455a8a9857a78c419f113c3071e2d8a52d34e64
goodven152 <75521339+goodven152@users.noreply.github.com>
2025-02-21 18:10:27 +0200

    Update analysisTXT.dart

--- education/additional_loops/analysisTXT.dart
+++ education/additional_loops/analysisTXT.dart
@@ -1,3 +1,23 @@
 void main(){
+	int i = 0;
 
+	List<String> movies = [
+  'The Shawshank Redemption',
+  'The Godfather',
+  'The Dark Knight',
+  'Pulp Fiction',
+  'Schindler\'s List',
+  'The Lord of the Rings: The Return of the King',
+  'Forrest Gump',
+  'Inception',
+  'Fight Club',
+  'The Matrix'
+];
+
+	for(var movie in movies){
+		if(movie.length > 15) {
+			i++;
+		}
+	}
+  print('Количество фильмов имеющие более 15 символов: $i');
 }
