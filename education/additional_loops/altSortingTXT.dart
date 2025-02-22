05f0fd9cd453fd16f52b889db4a2a6f6e8649344
goodven152 <75521339+goodven152@users.noreply.github.com>
2025-02-22 16:52:28 +0200

    Update altSortingTXT.dart

--- education/additional_loops/altSortingTXT.dart
+++ education/additional_loops/altSortingTXT.dart
@@ -1,3 +1,18 @@
 void main(){
-
+		List<String> movies = [
+  'The Shawshank Redemption',
+  'The Godfather',
+  'The Dark Knight',
+  'Pulp Fiction',
+  'Schindler\'s List',
+  'The Lord of the Rings: The Return of the King',
+  'Forrest Gump',
+  'Inception',
+  'Fight Club',
+  'The Matrix'
+];
+		/// Проход по списку
+		for(int i = 0; i < movies.length; i++){
+			print(i);
+	}
 }
