31c0447450f04c0cc80662803111654bd2fe06ff
goodven152 <goodven152@gmail.com>
2025-02-24 15:58:20 +0300

    Remove commented-out code in AltSortingTxt.dart for clarity

--- education/AdditionalTasks/AltSortingTxt.dart
+++ education/AdditionalTasks/AltSortingTxt.dart
@@ -11,16 +11,6 @@ void main(){
   'Fight Club',
   'The Matrix'
 ];
-	// // 	/// Проход по списку
-	// // 	for(int i = 0; i < movies.length; i++){
-	// // 		print(i);
-	// // }
-  // for(var movie in movies){
-  //   print('Длинна элемента "$movie" равна : ${movie.length}');
-  // }
-
-  // // Нахождение по индексу
-  // print(movies[0]);
   for (int i = 0; i < movies.length - 1; i++) {
     for (int j = 0; j < movies.length - i - 1; j++) {
       if (movies[j].length > movies[j + 1].length) {
