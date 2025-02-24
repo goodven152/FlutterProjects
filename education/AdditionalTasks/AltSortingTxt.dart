c6842124f1be3d76205a3a288b49aa4e278f3700
goodven152 <goodven152@gmail.com>
2025-02-24 15:19:28 +0300

    Bubble sorting added

--- education/AdditionalTasks/AltSortingTxt.dart
+++ education/AdditionalTasks/AltSortingTxt.dart
@@ -11,8 +11,27 @@ void main(){
   'Fight Club',
   'The Matrix'
 ];
-		/// Проход по списку
-		for(int i = 0; i < movies.length; i++){
-			print(i);
-	}
+	// // 	/// Проход по списку
+	// // 	for(int i = 0; i < movies.length; i++){
+	// // 		print(i);
+	// // }
+  // for(var movie in movies){
+  //   print('Длинна элемента "$movie" равна : ${movie.length}');
+  // }
+
+  // // Нахождение по индексу
+  // print(movies[0]);
+  for (int i = 0; i < movies.length - 1; i++) {
+    for (int j = 0; j < movies.length - i - 1; j++) {
+      if (movies[j].length > movies[j + 1].length) {
+        // ft_swap
+        var temp = movies[j];
+        movies[j] = movies[j + 1];
+        movies[j + 1] = temp;
+      }
+    }
+  }
+  for(var movie in movies){
+    print(movie);
+  }
 }
