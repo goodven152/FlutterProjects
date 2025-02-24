066d76e5d7b70d38d4d2e9453fda0dd1715ba4ff
goodven152 <goodven152@gmail.com>
2025-02-24 20:14:45 +0300

    Добавление файла mapSortMaxScore.dart для сортировки студентов по наивысшей оценке

---
+++ education/AdditionalTasks/Sorting/mapSortMaxScore.dart
@@ -0,0 +1,22 @@
+void main() {
+
+List<Map<String, dynamic>> students = [
+    {'name': 'Алексей', 'score': 85},
+    {'name': 'Мария', 'score': 92},
+    {'name': 'Иван', 'score': 76},
+    {'name': 'Светлана', 'score': 90},
+    {'name': 'Дмитрий', 'score': 59},
+    {'name': 'Елена', 'score': 73},
+    {'name': 'Павел', 'score': 88},
+    {'name': 'Ольга', 'score': 95},
+    {'name': 'Сергей', 'score': 67},
+    {'name': 'Анна', 'score': 80},
+];
+    // для себя
+    print('Ученик из списка. Имя: ${students[4]['name']}\n Оценка: ${students[4]['score']}');
+
+    students.sort((a,b) => a['score'].compareTo(b['score']));
+    var topStudent = students[students.length - 1];
+    print('Ученик с наивысшей оценкой: ${topStudent['name']}\nОценка: ${topStudent['score']}');
+}
+
