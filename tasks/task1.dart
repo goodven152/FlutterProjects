3d8ee8598ce70ac2f62943752dcecf84233c346a
goodven152 <goodven152@gmail.com>
2025-02-28 23:42:54 +0300

    Добавление нового файла task1.dart для ввода имени и возраста пользователя с выводом информации о совершеннолетии

---
+++ tasks/task1.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+
+void main() {
+  print("Введите ваше имя:");
+  String? nameInput = stdin.readLineSync();
+  
+  print("Введите ваш возраст:");
+  String? ageInput = stdin.readLineSync();
+  int? age = int.tryParse(ageInput!);
+  if (age == null) {
+    print("Неправильный тип данных.");
+    return;
+  }
+  print("Привет, $nameInput! Тебе $age лет.");
+
+  if (age < 18) {
+    int yearsLeft = 18 - age;
+    print("До совершеннолетия осталось $yearsLeft лет.");
+  } else if (age >= 18) {
+    int yearsSince = age - 18;
+    print("После совершеннолетия прошло $yearsSince, лет");
+  } else {
+    print("Неправильный тип данных.");
+  }
+}
