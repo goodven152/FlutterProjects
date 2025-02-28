21411d1b26833bff7d04cc56ba553908d7768a47
goodven152 <goodven152@gmail.com>
2025-02-28 23:44:38 +0300

    Хз чё добавить

--- tasks/task1.dart
+++ tasks/task1.dart
@@ -1,25 +1,15 @@
 import 'dart:io';
+void main(){
+  var name = "Aleksei";
+  var age = 29;
+  print("Привет, $name! Тебе $age");
 
-void main() {
-  print("Введите ваше имя:");
-  String? nameInput = stdin.readLineSync();
-  
-  print("Введите ваш возраст:");
-  String? ageInput = stdin.readLineSync();
-  int? age = int.tryParse(ageInput!);
-  if (age == null) {
-    print("Неправильный тип данных.");
-    return;
-  }
-  print("Привет, $nameInput! Тебе $age лет.");
-
-  if (age < 18) {
-    int yearsLeft = 18 - age;
-    print("До совершеннолетия осталось $yearsLeft лет.");
-  } else if (age >= 18) {
+  if (age > 25) {
     int yearsSince = age - 18;
-    print("После совершеннолетия прошло $yearsSince, лет");
-  } else {
-    print("Неправильный тип данных.");
+    print("С момента совершеннолетия прошло $yearsSince лет");
+  } else if (age <= 18){
+    int yearsLeft = 18 - age;
+    print("До совершеннолетия осталось $yearsLeft лет");
   }
+
 }
