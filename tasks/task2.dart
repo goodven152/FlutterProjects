a281c890a470b57f99c7e5470a20e4786db3ba93
goodven152 <75521339+goodven152@users.noreply.github.com>
2025-03-01 13:42:53 +0300

    Create task2.dart

---
+++ tasks/task2.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+void main(){
+
+	Map<String> Users = {
+	'name' = 'Дмитрий, 'age' = 13;
+}
+  print("Введите ваше имя:");
+  String? nameInput = stdin.readLineSync();
+
+  print("Введите ваш возраст:");
+  String? ageInput = stdin.readLineSync();
+  int age = int.parse(ageInput!);
+
+  print("Привет, $nameInput. Тебе $ageInput");
+  if (age > 25) {
+    int yearsSince = age - 18;
+    print("С момента совершеннолетия прошло $yearsSince лет");
+  } else if (age <= 18){
+    int yearsLeft = 18 - age;
+    print("До совершеннолетия осталось $yearsLeft лет");
+  }
+}
